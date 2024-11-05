c34f6f4212cce788d18010607c908ec0bbe9d4cc
PurnaChanderPathi <pathipurnachander@gmail.com>
2024-11-05 17:22:12 +0530

    PPC

---
+++ FullStackMicroServicewithReact/AML Project/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/Header.js'
+import Amlak from './components/body/Amlak.js';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Amlak/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FullStackMicroServicewithReact/AML Project/frontend/src/components/body/Amlak.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Amlak.css'
+import HomePage from './HomePage'
+
+const Amlak = () => {
+  return (
+    <div className='mainDiv'>
+      <div className='Heading'>Amlak Home Page</div>
+      <div className='HomePage'>
+            <HomePage />
+      </div>
+    </div>
+  )
+}
+
+export default Amlak
